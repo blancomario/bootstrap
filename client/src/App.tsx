6544ad746ff2a81f408722e0f6f4e2d03e8728ae
Mario Blanco <blanco.mario@icloud.com>
2022-05-07 18:29:03 -0500

    - calling `hello` api - added proxy to package.json to avoid cross-origin errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,21 +3,30 @@ import logo from './logo.svg';
 import React from 'react';
 
 function App(): React.ReactElement {
+  const [serverMessage, setServerMessage] = React.useState();
+
+  React.useEffect(() => {
+    async function getMessage(): Promise<void> {
+      const response = await fetch('http://localhost:3000/api/hello');
+      const message = await response.json();
+      setServerMessage(message.message);
+    }
+
+    getMessage();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        Bootstrap
+      </header>
+      <section>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+        <p>Server message {serverMessage}</p>
+      </section>
     </div>
   );
 }
